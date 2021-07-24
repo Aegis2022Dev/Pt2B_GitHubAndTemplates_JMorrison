7950b17b71ff557c4414d90919997c062e16ca8a
Asgaard <Asgaard@DESKTOP>
2021-07-24 16:17:02 -0400

    upload main() server connectivity test

---
+++ Pt2B_GitHubAndTemplatesSource_JMorrison.cpp
@@ -0,0 +1,44 @@
+/*
+* Joy Morrison
+* CIS 1202
+* 07/15/2021
+*
+* Week 11 – Exception Classes Part 2B: Write User Defined Template Functions to Facilitate Math Functions for Numeric Data Types
+*
+* Program develops custom templated functions to perform math on various numeric data types
+* Program creates and uses several customized math template functions
+* Program will frquently utilize GitHub.com source management tool to push and commit changes to source code
+* Final program push will be submitted to GitHub for Instructor to clone
+*/
+
+#include <iostream> // allow use of cout/cin data stream objects
+#include <algorithm> //allow copying of arrays
+#include <fstream> // to enable input/output field handlers
+#include <iomanip> // to allow field formatting using setprecision, setw etc.
+#include <cstring>  // allow use of cstring library 
+#include <cctype> // allow use of isalpha, isdigit and other native boolean char/integer methods
+#include <ctime> // to make rand() generator dynamic
+#include <string>  // allow use of STRING library to use methods like getline()
+#include <cstdlib> //permit use of basic utility and math fx like transform()
+#include <cmath> // allow use of math library where necessary
+#include <cstdbool> // allow use of boolean data structures where necessary
+#include <cctype> //allow use of isdigit(arg) and other native boolean alphanumeric/char methods
+
+using namespace std;
+
+//DECLARE FORWARD DECLARATIONS, METHOD & STRUCTURES DEFINITIONS FOR FUNCTION PROTOTYPES
+
+
+/****BEGIN main() METHOD****/
+
+int main()
+{
+
+
+
+    system("pause");
+    return 0;
+
+}// End main()
+
+/****METHOD DEFINITIONS****/
