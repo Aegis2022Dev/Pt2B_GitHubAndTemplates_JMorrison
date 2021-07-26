274d42d7d86b4ccd3559615a051f3d3d2e7d30a9
Asgaard <Asgaard@DESKTOP>
2021-07-25 23:03:44 -0400

    corrected the logical error in the template function in line 37. Removed the round() as this was returning incorrect values for the double & float types. finally, added the variable types in the assignment sample output with appropriate formatting.

--- Pt2B_GitHubAndTemplatesSource_JMorrison.cpp
+++ Pt2B_GitHubAndTemplatesSource_JMorrison.cpp
@@ -5,10 +5,10 @@
 *
 * Week 11 – Exception Classes Part 2B: Write User Defined Template Functions to Facilitate Math Functions for Numeric Data Types
 *
-* Program develops custom templated functions to perform math on various numeric data types
-* Program creates and uses several customized math template functions
+* Program develops a custom templated function to perform rounding math on various numeric data types
+* Program creates and uses a customized math template function using the round() method from <cmath>
 * Program will frquently utilize GitHub.com source management tool to push and commit changes to source code
-* Final program push will be submitted to GitHub for Instructor to clone
+* Final program push will be submitted to GitHub for Instructor cloning and review
 */
 
 #include <iostream> // allow use of cout/cin data stream objects
@@ -34,7 +34,7 @@ template <typename T>
 T half(T number)
 {
     T result;
-    result = round(number /2);
+    result = number /2;
 
     return result;
 }
@@ -48,9 +48,11 @@ int main()
     double negArgX2 = -10.039364; // a predefined negative double argument
     double posArgX1 = 6.6; // a predefined positive double argument
     double posArgX2 = 7.039364; // a predefined positive double argument
+    double a = 7.0;
 
     float X = 2.61; // a predefined positive float argument
     float Y = 7.16; // a predefined postive float argument
+    float b = 5.0f;
 
     int arg1 = 3; // a predefined positive integer argument
     float argZ1 = static_cast<float> (arg1); // forcibly convert argZ into float so resulting 
@@ -61,11 +63,14 @@ int main()
     int arg3 = -3;
     float argZ3 = static_cast<float> (arg3); // a predefined negative integer argument
 
+
+    int c1 = 3;
+    float c = static_cast<float> (c1);
+
     //call the template function for the 3 different variable type parameter cases
 
     cout << "\nHalf values for \n\n";
-    //cout << fixed << showpoint;
-    //cout << setprecision(1);
+
     cout << "\t" << "negative double type: " << negArgX2 << " rounds down to " << fixed << showpoint << setprecision(4) << half(negArgX2) << endl;
     cout << "\t" << "negative double type: " << negArgX1 << " rounds up to " << fixed << showpoint << setprecision(4) << half(negArgX1) << endl;
     cout << "\t" << "positive double type: " << posArgX1 << " rounds down to  " << fixed << showpoint << setprecision(4) << half(posArgX1) << endl;
@@ -81,7 +86,12 @@ int main()
     cout << "\t" << "negative integer type: " << arg3 << " rounds down to " << fixed << showpoint << setprecision(0) << half(argZ3) << endl;
     cout << "\n\n";
 
-
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    cout << "\nThis is the final test of the output provided in the Assignment.\n\n";
+    cout << "\t" << "Half value for double type: " << a << " is " << fixed << showpoint << setprecision(1) << half(a) << endl;
+    cout << "\t" << "Half value for float type: " << b << " is  " << fixed << showpoint << setprecision(1) << half(b) << endl;
+    cout << "\t" << "Half vaule for integer type: " << c1 << " rounds up to " << fixed << showpoint << setprecision(0) << half(c) << endl;
+    cout << "\n\n";
 
     system("pause");
     return 0;
